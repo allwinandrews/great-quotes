9374260251a72571b6fc211171e341d827cb1d15
Andrews <allwinandrews@gmail.com>
2021-07-08 10:07:33 +0530

    Outputs Data on the QuoteDetail Page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,34 @@
 import React from "react";
 import { useParams, Route } from "react-router-dom";
 
+import HighLightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Allwin",
+    text: "Allwin Rules",
+  },
+  {
+    id: "q2",
+    author: "Max",
+    text: "Learning React is fun",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
+
   return (
     <>
-      <h1>QuoteDetail</h1>
-      <p>{params.quoteId}</p>
+      <HighLightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
