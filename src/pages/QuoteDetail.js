568959a4cda5c3a3ecd194ca9bc055a4834e302a
Andrews <allwinandrews@gmail.com>
2021-07-08 12:56:15 +0530

    Display Comments based on route

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
 import HighLightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
@@ -29,6 +29,13 @@ const QuoteDetail = () => {
   return (
     <>
       <HighLightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
